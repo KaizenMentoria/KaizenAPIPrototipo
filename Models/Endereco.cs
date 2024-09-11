d6da66283d8879c9deacfc9b9dce069c905e0fb4
Lucas Gabriel Castro <lucasgabrielcastro@outlook.com>
2024-09-11 17:14:47 -0300

    fix(Endereco): Corrigido declaracao de classe dentro de outra classe com mesmo nome

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -7,16 +7,13 @@ namespace KaizenAPI.Models
 {
     public class Endereco
     {
-        public class Endereco
-        {
-            public string CEP { get; set; }
-            public string Logradouro { get; set; }
-            public string Numero { get; set; }
-            public string Complemento { get; set; }
-            public string Bairro { get; set; }
-            public string Localidade { get; set; }
-            public string UF { get; set; }
-            public string DDD { get; set; }
-        }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string DDD { get; set; }
     }
 }
