274fe98f607746f6854a2cba3cddfc54fa2a35ed
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 17:31:47 -0300

    feat(DTOs): Renomear classe UpdateEnderecoDTO para UpdateEnderecoDto

--- DTOs/Endereco/UpdateEnderecoDTO.cs
+++ DTOs/Endereco/UpdateEnderecoDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace KaizenAPI.DTOs.Endereco
 {
 
-    public record UpdateEnderecoDTO(
+    public record UpdateEnderecoDto(
         string Cep,
         string Numero,
         string Complemento
