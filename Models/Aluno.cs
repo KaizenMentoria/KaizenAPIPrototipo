8de0c4a4762a8b2572161cc4f90ea12520774953
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 17:32:32 -0300

    feat(Aluno): Adicionar propriedade Ativo e ajustar propriedades de Pessoa em Aluno

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -4,42 +4,55 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using KaizenAPI.DTOs.Aluno;
+using KaizenAPI.DTOs.Endereco;
+using KaizenAPI.DTOs.Pessoa;
+using KaizenAPI.DTOs.ContatoEmergencia;
 
 namespace KaizenAPI.Models
 {
     public class Aluno : Pessoa
     {
         public Guid Id { get; set; }
-        public bool Ativo { get; set; } = true;
+        public bool Ativo { get; set; }
         public string DiasAulasContratados { get; set; }
         public DateTime DataNascimento { get; set; }
-        public Endereco Endereco { get; set; }
+        public new Endereco Endereco { get; set; }
         public ContatoEmergencia ContatoEmergencia { get; set; }
         public Responsavel Responsavel { get; set; }
         public Cobranca Cobranca { get; set; }
         public ICollection<Turma> Turmas { get; set; }
         public ICollection<Aula> Aulas { get; set; }
         public ICollection<DiarioBordo> DiariosBordo { get; set; }
-    }
-    public Aluno(CreateAlunoDto novoAluno)
+
+        public Aluno(CreateAlunoDto novoAluno)
         {
             Id = Guid.NewGuid();
             Ativo = novoAluno.Ativo;
-            PrimeiroNome = novoAluno.pessoa.PrimeiroNome;
-            SegundoNome = novoAluno.pessoa.SegundoNome;
-            Sobrenome = novoAluno.pessoa.Sobrenome;
-            Cpf = novoAluno.pessoa.Cpf;
-            Rg = novoAluno.pessoa.Rg;
+
+            // Acessando as propriedades do objeto Pessoa no DTO
+            PrimeiroNome = novoAluno.Pessoa.PrimeiroNome;
+            SegundoNome = novoAluno.Pessoa.SegundoNome;
+            Sobrenome = novoAluno.Pessoa.Sobrenome;
+            Cpf = novoAluno.Pessoa.Cpf;
+            Rg = novoAluno.Pessoa.Rg;
+
+            // Definindo as propriedades restantes
             DataNascimento = novoAluno.DataNascimento;
             ContatoEmergencia = novoAluno.ContatoEmergencia;
-            Responsavel = novoAluno.Responsavel;
-            Cobranca = novoAluno.Cobranca;
-            Endereco = novoAluno.Endereco;  // Nova propriedade Endereço
             DiasAulasContratados = novoAluno.DiasAulasContratados;
 
+            // Aqui você pode criar a instância de Endereco a partir de um UpdateEnderecoDto, se necessário
+            Endereco = new Endereco
+            {
+                Cep = novoAluno.Endereco.Cep,
+                Numero = novoAluno.Endereco.Numero,
+                Complemento = novoAluno.Endereco.Complemento
+            };
+
             // Inicializando as coleções
-            Turmas = novoAluno.Turmas ?? new List<Turma>();  // Inicializar com os dados fornecidos ou uma nova lista
-            Aulas = novoAluno.Aulas ?? new List<Aula>();     // Inicializar com os dados fornecidos ou uma nova lista
-            DiariosBordo = novoAluno.DiariosBordo ?? new List<DiarioBordo>(); // Inicializar com os dados fornecidos ou uma nova lista
+            Turmas = new List<Turma>();
+            Aulas = new List<Aula>();
+            DiariosBordo = new List<DiarioBordo>();
         }
     }
+}
