998349259d69bb2612526a5581b9a6425fab2116
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 17:33:20 -0300

    feat(Models): Mover Coordenador para o namespace KaizenAPI.Models

--- Models/Coordenador.cs
+++ Models/Coordenador.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PrototipoKaizen.Models
+namespace KaizenAPI.Models
 {
     public class Coordenador : Pessoa
     {
