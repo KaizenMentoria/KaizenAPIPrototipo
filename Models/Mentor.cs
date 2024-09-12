3d30942f2c19ad9523d5b6d027bbb5a6429ebd9b
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 15:43:09 -0300

    feat(Models): Adicionada classe Mentor com propriedades e métodos

---
+++ Models/Mentor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototipoKaizen.Models
+{
+    public class Mentor : Pessoa
+    {
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public ContatoEmergencia? ContatoEmergencia { get; set; }
+        public InformacoesPagamento? InformacoesPagamento { get; set; }
+        public List<Turma>? Turmas { get; set; }
+        public List<Aluno>? Alunos { get; set; }
+        public List<Aula>? Aulas { get; set; }
+        public List<DiarioBordo>? DiariosBordo { get; set; }
+        public List<Formacoes>? Formacoes { get; set; }
+        public string? TipoMentor { get; set; }
+    }
+}
