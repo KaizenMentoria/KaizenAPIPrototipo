51e0f84818794a839e6dc691dd0f6c6895cc4885
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 17:31:38 -0300

    feat(Aluno): Adicionado DTO CreateAlunoDto com propriedades e métodos

--- DTOs/Aluno/CreateAlunoDTO.cs
+++ DTOs/Aluno/CreateAlunoDTO.cs
@@ -2,20 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KaizenAPI.DTOs.Pessoa;
+using KaizenAPI.DTOs.ContatoEmergencia;
 
 namespace KaizenAPI.DTOs.Aluno
 {
 
-    public record CreateAlunoDTO
+    public record CreateAlunoDto
     (
-        string Nome,
-        string Email,
-        string Cpf,
-        string DataNascimento,
-        string Telefone,
-        string Cep,
-        string Complemento,
-        string Numero
+        PessoaDto Pessoa,
+        ContatoEmergenciaDto ContatoEmergencia,
+        string DiasAulasContratados,
+        DateTime DataNascimento,
+        bool Ativo = true
     );
-
 }
