4752faecae7ac04ff653174bd0589de3364316c4
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 15:43:14 -0300

    feat(Models): Adicionada classe Pessoa com propriedades e métodos

---
+++ Models/Pessoa.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototipoKaizen.Models
+{
+    public class Pessoa
+    {
+        // Informações pessoais
+        public string? PrimeiroNome { get; set; }
+        public string? SegundoNome { get; set; }
+        public string? Sobrenome { get; set; }
+        public string? Cpf { get; set; }
+        public string? Rg { get; set; }
+        // Informações de contato
+        public Endereco? Endereco { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
+    }
+}
