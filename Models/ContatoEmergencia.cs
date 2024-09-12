87da94f48668b13fe26b2bb53e1f3b1521354441
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 17:33:16 -0300

    feat(Models): Mover ContatoEmergencia para o namespace KaizenAPI.Models e adicionar referência a KaizenAPI.DTOs.ContatoEmergencia

--- Models/ContatoEmergencia.cs
+++ Models/ContatoEmergencia.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KaizenAPI.DTOs.ContatoEmergencia;
 
-namespace PrototipoKaizen.Models
+namespace KaizenAPI.Models
 {
     public class ContatoEmergencia :Pessoa
     {
@@ -11,5 +12,12 @@ namespace PrototipoKaizen.Models
         public bool Ativo { get; set; }
         public Guid IdPessoa { get; set; }
         public string? NomePessoa { get; set; }
+    
+        public ContatoEmergencia(ContatoEmergenciaDto dto)
+        {
+            NomePessoa = dto.Nome;
+
+            ContatoEmergencia.Telefone = dto.Telefone;
+        }
     }
 }
