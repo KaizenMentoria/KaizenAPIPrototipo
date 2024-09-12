fac2786580359cfd287fd8af34528a43d89754cf
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 17:32:54 -0300

    feat(Models): Adicionar namespace KaizenAPI.Enums no arquivo Aula.cs

--- Models/Aula.cs
+++ Models/Aula.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using PrototipoKaizen.Enums;
+using KaizenAPI.Enums;
 
-namespace PrototipoKaizen.Models
+namespace KaizenAPI.Models
 {
     public class Aula
     {
