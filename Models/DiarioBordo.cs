aaffd636f0b2a6c6e286d6ba69b0f561b2e8e70b
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 15:42:55 -0300

    feat(Models): Adicionada classe DiarioBordo com propriedades e métodos

---
+++ Models/DiarioBordo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototipoKaizen.Models
+{
+    public class DiarioBordo
+    {
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public Aluno? Aluno { get; set; }
+        public Mentor? Mentor { get; set; }
+        public Aviso? Aviso { get; set; }
+        public string? ProximaAula { get; set; }
+    }
+}
