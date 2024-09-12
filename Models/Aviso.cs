2b6cf8b4dd345cbd183d2800bc60b9d070ebf1e0
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-12 15:42:39 -0300

    feat(Models): Adicionada classe Aviso com propriedades e métodos

---
+++ Models/Aviso.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototipoKaizen.Models
+{
+    public class Aviso
+    {
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public Guid IdCriador { get; set; }
+        public string? Titulo { get; set; }
+        public string? DescricaoNoticia { get; set; }
+    }
+}
