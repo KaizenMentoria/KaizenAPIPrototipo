f259cf4db4e35fd89d44b56e6f13b4c450ecaca9
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-17 14:36:11 -0300

    feat: Adicionar modelo Mentor

---
+++ Models/Mentor.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using KaizenAPIPrototipo.Models;
+
+namespace KaizenAPIPrototipo.Models
+{
+    public class Mentor : Pessoa
+    {
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string TipoMentor { get; set; }
+        public ContatoEmergencia ContatoEmergencia { get; set; }
+        public Coordenador Coordenador { get; set; }
+        public List<Formacao> Formacoes { get; set; }
+        public ICollection<Aluno> Alunos { get; set; }
+        public List<Aula> Aulas { get; set; }
+        public List<DiarioBordo> DiariosBordo { get; set; }
+        public InformacoesPagamento InformacoesPagamento { get; set; }
+    }
+}
