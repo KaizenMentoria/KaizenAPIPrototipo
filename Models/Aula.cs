2568f372ff80317e239555537fe26dc28d9b04f4
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-17 14:35:38 -0300

    feat: Adicionar modelo Aula
    Adiciona o modelo Aula com propriedades relacionadas a uma aula, como data, horário, tipo, coordenador, mentor, alunos e presenças.

---
+++ Models/Aula.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KaizenAPI.Models;
+using KaizenAPIPrototipo.Enums;
+
+namespace KaizenAPIPrototipo.Models
+{
+    public class Aula
+    {
+        public Guid Id { get; set; }
+        public bool Ativa { get; set; }
+        public DateTime DataAula { get; set; }
+        public TimeSpan HorarioInicio { get; set; }
+        public TimeSpan HorarioFim { get; set; }
+        public TipoAula TipoAula { get; set; }
+        public Coordenador Coordenador { get; set; }
+        public Mentor Mentor { get; set; }
+        public ICollection<Aluno> Alunos { get; set; }
+        public ICollection<Presenca> PresencasAlunos { get; set; }
+        public Presenca PresencaMentor { get; set; }
+    }
+}
