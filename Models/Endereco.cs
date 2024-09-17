153d90ed6fb28d5b20108046748bc1a29832d074
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-17 14:36:00 -0300

    refactor: Atualizar propriedade CEP para Cep no modelo Endereco

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -7,13 +7,8 @@ namespace KaizenAPI.Models
     public class Endereco
     {
         public Guid Id { get; set; }
-        public string CEP { get; set; }
-        public string Logradouro { get; set; }
+        public string Cep { get; set; }
         public string Numero { get; set; }
         public string Complemento { get; set; }
-        public string Bairro { get; set; }
-        public string Localidade { get; set; }
-        public string UF { get; set; }
-        public string DDD { get; set; }
     }
 }
