de4631235bac6d203e50f9fe0d05f250e8114eee
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-18 15:56:25 -0300

    feat: Adicionar classe Cobranca ao namespace KaizenAPIPrototipo.Models

---
+++ Models/Cobranca.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KaizenAPIPrototipo.Models
+{
+    public class Cobranca
+    {
+        public Guid Id { get; set; }
+        public bool Pago { get; set; }
+        public TipoPlano TipoPlano { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public double Valor { get; set; }
+        public string Descricao { get; set; }
+        public Endereco EnderecoDeCobranca { get; set; }
+        public FormaPagamento FormaDePagamento { get; set; }
+    }
+}
