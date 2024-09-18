08c1fdf8e031e0bec48c619c4966de1223f1df80
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-18 15:56:47 -0300

    Refatorar namespace para KaizenAPIPrototipo.Models em Models/Mentor.cs

--- Models/Mentor.cs
+++ Models/Mentor.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using KaizenAPIPrototipo.Models;
 
 namespace KaizenAPIPrototipo.Models
 {
