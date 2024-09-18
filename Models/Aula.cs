7ef81b500acdeeb7319404e0e5c3138c76fdec86
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-18 15:56:19 -0300

    Refatorar namespace para KaizenAPIPrototipo.Models

--- Models/Aula.cs
+++ Models/Aula.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using KaizenAPI.Models;
+using KaizenAPIPrototipo.Models;
 using KaizenAPIPrototipo.Enums;
 
 namespace KaizenAPIPrototipo.Models
