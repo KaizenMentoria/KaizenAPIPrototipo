a1bae4a61a45d636e2c4e68927ec1abb261a5a7b
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-09-18 15:56:07 -0300

    Refatorar namespaces para KaizenAPIPrototipo.Data

---
+++ Migrations/20240918180734_v2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KaizenAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PrimeiroNome = table.Column<string>(type: "text", nullable: false),
+                    SegundoNome = table.Column<string>(type: "text", nullable: true),
+                    Sobrenome = table.Column<string>(type: "text", nullable: false),
+                    Cpf = table.Column<string>(type: "text", nullable: false),
+                    Rg = table.Column<string>(type: "text", nullable: false),
+                    Telefone = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Endereco_Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Endereco_Cep = table.Column<string>(type: "text", nullable: false),
+                    Endereco_Numero = table.Column<string>(type: "text", nullable: false),
+                    Endereco_Complemento = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alunos_Enderecos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Cep = table.Column<string>(type: "text", nullable: false),
+                    Numero = table.Column<string>(type: "text", nullable: false),
+                    Complemento = table.Column<string>(type: "text", nullable: false),
+                    AlunoId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos_Enderecos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alunos_Enderecos_Alunos_AlunoId",
+                        column: x => x.AlunoId,
+                        principalTable: "Alunos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alunos_Enderecos_AlunoId",
+                table: "Alunos_Enderecos",
+                column: "AlunoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alunos_Enderecos");
+
+            migrationBuilder.DropTable(
+                name: "Alunos");
+        }
+    }
+}
